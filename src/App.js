c1d5de7a4489e2c6aef1cfa644735142546e6c24
Bilal <bilal.se.iiui@gmail.com>
2022-10-21 04:46:58 -0700

    added all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Add from "./Component/Add";
+import Header from "./Component/Header";
+import Watched from "./Component/Watched";
+import Watchlist from "./Component/Watchlist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <Routes>
+        <Route path="/watched" element={<Watched />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/add" element={<Add fauji="fauji" />} />
+        <Route path="/add" element={<Add fauji="fauji" />} />
+        <Route path="/add" element={<Add fauji="fauji" />} />
+      </Routes>
+    </Fragment>
   );
 }
 
